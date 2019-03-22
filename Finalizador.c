fd478da2e416e1c44a7681c6ae63ada2e270fd50
Nathalia3 <natgondu@gmail.com>
2019-03-21 23:32:44 -0600

    Finalizador structure

---
+++ Finalizador.c
@@ -0,0 +1,42 @@
+#include "Finalizador.h"
+
+
+int main(int argc, char **argv) {
+	//Agregar validaciones
+	
+}
+
+
+int  Inicia_MemoriaCompartida(const char **nombre_buffer){
+
+}
+
+
+
+//Crea un nuevo mensaje en el buffer 
+void Crea_Mensaje(int pid){
+
+}
+
+
+void Cancela_procesos(){
+	
+		//if((contador_productores==0) && (rptr->contador_consumidores==0)){
+			//Imprime_Stats();
+            //investigate destroy
+
+
+}
+
+void Imprime_Stats(){
+               
+                fprintf (stderr, "Cancelando el Sistema de Procesos... \n\n");
+                fprintf (stderr, "Proceso:   \n");
+                fprintf (stderr, "Total de consumidores activos: \n");
+                fprintf (stderr, "Total de productores: \n");
+                fprintf (stderr, "Tamaño del buffer: \n");
+                fprintf (stderr, "Total de consumidores durante la ejecución: \n");
+                fprintf (stderr, "Total de mensajes: \n");
+
+}
+
---
+++ Finalizador.h
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Buffer.h"
+#include "Mensaje.h"
+
+const int OFF=0;
+const int ERROR=0;
+struct buffer *buf;
+int fd;
+int tamano_buffer;
+
+int Inicia_MemoriaCompartida(const char *nombre_buffer);
+void Crea_Mensaje(int pid);
+void Cancela_procesos();
+void Imprime_Stats();
