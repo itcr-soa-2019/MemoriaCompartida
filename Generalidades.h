1b00fede004e976bcbba74e4f0ef107546a2480c
Nathalia3 <natgondu@gmail.com>
2019-03-27 21:24:19 -0600

    Creador changes

--- Generalidades.h
+++ Generalidades.h
@@ -1,3 +1,3 @@
 // metodos para validar los parametros de entrada
 int Validar_Parametros(int argc, char **argv, char* Nombre, char* Argumento);
-void uso( int exitval, char* nombre, char* argumento, char* error);
+void indicaciones( int exitval, char* nombre, char* argumento, char* error);
